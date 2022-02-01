dd6bf90200628762a996e6f36cf109c5c31f02e3
Yuliia Danylenko <yvdanilenko@gmail.com>
2022-02-02 00:02:12 +0200

    create repo

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class App extends Component {
   state = { good: 0, neutral: 0, bad: 0 };
   render() {
-    return <div> HELLO </div>;
+    return <div> images </div>;
   }
 }
 
